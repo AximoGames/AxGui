2262135ca925ea0d61c9299d6b4018346551e699
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-11-09 22:58:35 +0100

    added point

---
+++ src/AxGui/Point.cs
@@ -0,0 +1,50 @@
+﻿// This file is part of AxGUI. Web: https://github.com/AximoGames
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace AxGui
+{
+    public struct Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return X;
+                    case 1:
+                        return Y;
+                    default:
+                        throw new ArgumentException(nameof(index));
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        X = value;
+                        break;
+                    case 1:
+                        Y = value;
+                        break;
+                    default:
+                        throw new ArgumentException(nameof(index));
+                }
+            }
+        }
+    }
+
+}
